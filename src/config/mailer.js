9ec52bdc68334a7914e1a0906b65099c10519d71
Miranda Mateus <cerranetai@gmail.com>
2025-11-10 22:01:47 -0300

    Funcao de esqueci a senha4

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -286,5 +286,6 @@ module.exports = {
     sendConfirmationEmail, // Agora adaptada
     sendUpdateEmail,       // Agora adaptada
     sendInvoiceNotificationEmail,
-    sendReceiptUploadNotificationEmail
+    sendReceiptUploadNotificationEmail,
+    sendPasswordResetEmail
 };
